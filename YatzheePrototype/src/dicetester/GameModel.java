44623501de005588849625e099183b1bde24d686
elijahambrosegreen <elijahambrosegreen@gmail.com>
2024-10-10 11:37:50 -0700

    started gameModel class

---
+++ YatzheePrototype/src/dicetester/GameModel.java
@@ -0,0 +1,10 @@
+package dicetester;
+/**
+ *
+ * @author 507606
+ */
+public class GameModel 
+{
+    public final static int NUM_UPPER_SCORE_CATS = 6;
+    public final static int NUM_LOWER_SCORE_CATS = 7;
+}
